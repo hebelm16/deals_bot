de22a36df7d9827fcb9ef29a737867d022f271f5
hebelm16 <hebelm16@gmail.com>
2024-07-30 13:58:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import asyncio
-import logging
-from ofertasbot import OfertasBot
-
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+from ofertasbot import main
 
 if __name__ == "__main__":
-    bot = OfertasBot()
-    asyncio.run(bot.run())
+    main()
